62612cd42198c0be6ed2a1c50075e78f287065f5
Kamali Hicks <kamalikh3@gmail.com>
2025-02-01 19:44:43 -0500

    Task 4 Do...While Loop

--- cc_4.js
+++ cc_4.js
@@ -4,7 +4,7 @@ let totalAmount = purchaseAmount; // totalAmount after discount
 if (purchaseAmount > 100) {
     totalAmount = purchaseAmount * 0.20;
 } // Determine if purchase amount is eligible for discount & calculate total amount after discount 
-console.log(`Final amount after discount: $${totalAmount}`); // Log final amount to console
+console.log(`Final amount after discount: ${totalAmount}`); // Log final amount to console
 
 // Task 2 For Loop 
 
@@ -13,11 +13,19 @@ let totalSales = 0; //
 for (let i = 0; i < sales.length; i++) {
    totalSales += sales[i];
 } // Calculate total sales
-console.log(`Total Sale Amount: $${totalSales}`); // Log total sale amount to console
+console.log(`Total Sale Amount: ${totalSales}`); // Log total sale amount to console
 
 // Task 3 While Loop
 let stock = 10; 
 while (stock > 0) {
     console.log("Stock is", stock);
-    count--; 
+    stock--; 
 } // Decrease count by 1 until stock reaches 0
+
+// Task 4 Do... While Loop
+let responses = 0;
+do {
+    console.log(`User Response: ${responses}`)
+    responses ++;
+
+} while (response < 3) // Collect user responses until response count reaches 3 
