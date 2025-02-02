5659998d9397bcc186cfd89c0d4ca8a14da33c8f
Kamali Hicks <kamalikh3@gmail.com>
2025-02-01 19:12:19 -0500

    Task 1 If Statements

---
+++ cc_4.js
@@ -0,0 +1,6 @@
+// Task 1 If Statements
+let purchaseAmount = 4900;
+if (purchaseAmount > 100) {
+    totalAmount = purchaseAmount * 0.20;
+}
+console.log('Final amount after discount: $${totalAmount}')
